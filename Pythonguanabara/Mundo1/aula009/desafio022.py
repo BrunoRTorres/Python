ea30ce91de220a04e606b0d4e6665bc2db34603a
Bruno Ribas Torres <brunotorreseng@gmail.com>
2025-04-10 13:37:23 -0300

    Cont Aula 09 + desafios

---
+++ Pythonguanabara/Mundo1/aula009/desafio022.py
@@ -0,0 +1,11 @@
+nome = str(input('Digite seu nome: '))
+
+print(nome.upper())
+
+print(nome.lower())
+
+# todo quantas letras ao todo sem considerar espacos
+
+dividido = nome.split()
+
+print(len(dividido[0]))
