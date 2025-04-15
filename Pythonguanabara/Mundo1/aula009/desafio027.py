a17eeb9460ee70769636ba5ae51d4ab9c3be9185
Bruno Ribas Torres <brunotorreseng@gmail.com>
2025-04-15 11:10:03 -0300

    termino aula009

---
+++ Pythonguanabara/Mundo1/aula009/desafio027.py
@@ -0,0 +1,7 @@
+nome = str(input('Digite o seu nome completo: '))
+
+nomeSplit = nome.split()
+
+print('O seu primeiro nome: {}'.format(nomeSplit [0]))
+
+print('O seu ultimo nome: {}'.format(nomeSplit [len(nomeSplit) -1]))
