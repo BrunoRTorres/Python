b7bee457d7162d5e2f27ee8d36d4cb2af1b6d8dd
Bruno Ribas Torres <brunotorreseng@gmail.com>
2025-05-13 14:53:01 -0300

    Aula015
    Termino aula
    inicio exercicios e desafios

--- Pythonguanabara/Mundo2/Aula015/desafio068.py
+++ Pythonguanabara/Mundo2/Aula015/desafio068.py
@@ -1,17 +1,51 @@
-from random import randint, choice
+from random import randint
 
 soma = cont = 0
 
-parimpar = ''
-
-computador = randint(1, 5)
-
-computadorparimpar = choice(['P', 'I'])
+parimpar = ' '
 
 while True:
 
+    print('=-' * 10 + '=')
+
     jogador = int(input('Jogue um numero de 1 a 5: '))
 
-    parimpar = str(input('Par ou Impar? [P/I] ')).upper().strip()[0]
+    print('=-' * 10 + '=')
 
-    
+    parimpar = str(input('Par ou Impar? [P/I] ')).upper().strip()
+
+    computador = randint(0, 5)
+
+    soma = jogador + computador
+
+    print('=-' * 10 + '=')
+
+    print(f'Voce escolheu {jogador} e o computador {computador} e deu {soma}')
+
+    print('=-' * 10 + '=')
+
+    if soma % 2 == 0:
+        
+        print('Deu par!', end=' ')
+
+    else:
+
+        print('Deu impar!', end=' ')
+
+    if parimpar == 'P' and soma % 2 == 0:
+
+        print('\033[32mVoce ganhou!\033[m')
+
+        cont += 1
+
+    else: 
+
+        print('\033[31mVoce perdeu!\033[m')
+        
+        break
+
+print('=-' * 10 + '=')
+
+print(f'Voce venceu {cont} partida(s) seguida(s)!')
+
+print('=-' * 10 + '=')
