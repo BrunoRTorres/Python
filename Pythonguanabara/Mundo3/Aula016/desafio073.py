a7672730884c4972eb796cad6787400fdf23ccb1
Bruno Ribas Torres <brunotorreseng@gmail.com>
2025-05-19 22:20:39 -0300

    Aula 16
    Realizando os desafios

--- Pythonguanabara/Mundo3/Aula016/desafio073.py
+++ Pythonguanabara/Mundo3/Aula016/desafio073.py
@@ -1,7 +1,19 @@
 time = ('Palmeiras', 'Flamengo', 'Cruzeiro', 'Bragantino', 'Ceara SC', 'Bahia', 'Fuminense', 'Corinthians', 'Atletico-MG', 'Botafogo', 'Sao Paulo', 'Mirassol', 'Vasco da Gama', 'Fortaleza', 'Internacional', 'EC Vitoria', 'Gremio', 'Juventude', 'Santos', 'Sport Recife')
 
-print(time[0:5])
+print('=' * 90)
 
-print(time[16:])
+print(f'Os 5 primeiros times sao: {time[0:5]}.')
 
-print()
+print('=' * 90)
+
+print(f'Os 4 ultimos times sao: {time[16:]}.')
+
+print('=' * 90)
+
+print(f'Os times em ordem alfabetica: {sorted(time)}.')
+
+print('=' * 90)
+
+print(f'O Botafogo esta na , {time.index('Botafogo') + 1}ª posicao.')
+
+print('=' * 90)
