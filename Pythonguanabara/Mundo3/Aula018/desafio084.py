8bbc24567a8f554424f99e140326ac7a44008841
Bruno Ribas Torres <brunotorreseng@gmail.com>
2025-06-05 15:23:38 -0300

    Aula 018
    Cont desafios e exercicios

--- Pythonguanabara/Mundo3/Aula018/desafio084.py
+++ Pythonguanabara/Mundo3/Aula018/desafio084.py
@@ -1,14 +1,20 @@
 pessoas = list()
 dados = list()
-totpessoas = 0
+maior = menor = 0
 
 while True:
    
     dados.append(str(input('Nome: ')))
     dados.append(float(input('Peso: ')))
+    if len(pessoas) == 0:
+        maior = menor = dados[1]
+    else:
+        if dados[1] > maior:
+            maior = dados[1]
+        if dados[1] < menor:
+            menor = dados[1]
     pessoas.append(dados[:])
     dados.clear()
-    totpessoas += 1
 
     saida = ' '
     while saida not in 'SN':
@@ -16,5 +22,16 @@ while True:
     if saida == 'N':
         break
 
-print(f'Foram cadastradas no total {totpessoas} pessoas.')
-print(f'O maior peso foi')
+print(f'Foram cadastradas no total {len(pessoas)} pessoas.')
+
+print(f'O maior peso foi de {maior}Kg. Peso de ', end='')
+for pessoa in pessoas:
+    if pessoa[1] == maior:
+        print(f'{pessoa[0]} ', end='')
+print()
+
+print(f'O menor peso foi de {menor}Kg. Peso de ', end='')
+for pessoa in pessoas:
+    if pessoa[1] == menor:
+        print(f'{pessoa[0]} ', end='')
+print()
