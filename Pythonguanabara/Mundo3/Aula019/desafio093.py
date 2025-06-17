80629ff49d66d96f942421d184b979da4ec158c0
Bruno Ribas Torres <brunotorreseng@gmail.com>
2025-06-16 21:32:17 -0300

    Aula 019
    Cont exercicios e desafios

--- Pythonguanabara/Mundo3/Aula019/desafio093.py
+++ Pythonguanabara/Mundo3/Aula019/desafio093.py
@@ -1,14 +1,26 @@
 jogador = dict()
+gols = list()
+
+print('=-' * 12 + '=')
 
 jogador['nome'] = str(input('Nome do jogador: ')).title()
-jogador['total'] = 0
+partidas = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
 
-partidas = int(input('Quantas partidas ele jogou? '))
 for i in range(0, partidas):
-    jogador[f'partida{i+1}'] = int(input(f'Quantos gols foram feitos na partida {i+1}? '))
-    jogador['total'] = jogador['total'] + jogador[f'partida{i+1}']
+    gols.append(int(input(f'Quantos gols {jogador["nome"]} fez na partida {i+1}? ')))
+jogador['gols'] = gols[:]
 
-print(f'O jogador {jogador["nome"]} jogou {partidas} partidas.')
-#for k, v in jogador.items()
-print(f'No total fez {jogador["total"]} gols.')
+total = 0
+for g in gols:
+    total += g
+jogador['total'] = total
 
+print('=-' * 16 + '=')
+
+print(f'O jogador {jogador["nome"]} jogou {len(gols)} partidas.')
+for p, g in enumerate(jogador['gols']):
+    print(f'Na partida {p+1} fez {g} gols.')
+
+print(f'O jogador {jogador["nome"]} marcou um total de {jogador["total"]} gols.')
+
+print('=-' * 21 + '=')
