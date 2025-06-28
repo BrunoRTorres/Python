0bde98c1d9aea3dcf99f8b68a77f209862ccd02b
Bruno Ribas Torres <150205572+BrunoRTorres@users.noreply.github.com>
2025-06-28 00:09:01 -0300

    Aula 021
    desafios feitos - 102, 103
    
    exercicios feitos - 101, 102, 102

---
+++ Pythonguanabara/Mundo3/Aula021/desafio103.py
@@ -0,0 +1,16 @@
+def ficha(nome='<desconhecido>', gols=0):
+    print(f'O jogador {nome}, fez {gols}(s) no campeonato.')
+    
+n = str(input('Nome do jogador: '))
+g = str(input('Quantos gols ele fez? '))
+
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+
+if n.strip() == '':
+    ficha(gols=g)
+else:
+    ficha(n, g)
+
