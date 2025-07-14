cc3d84fa76f09e69f17f284310f43ea26023c475
Bruno Ribas Torres <150205572+BrunoRTorres@users.noreply.github.com>
2025-07-13 23:42:57 -0300

    Aula 15 a 20
    Termino aula(s): 15 a 19
    
    Aula em andamento: 20

---
+++ Udemy_LuizOtavio/21_operador_and.py
@@ -0,0 +1,11 @@
+# Operadores logicos
+# and (e) or (ou) not (nao)
+# and - todas as concicoes precisam ser
+# verdadeiras.
+# Se qualquer valor for considerado falso,
+# a expressao inteira sera avaliada naquele valor 
+# Sao considerados falsy 
+# 0 0.0 '' False
+# Tambem existe o tipo None que e 
+# usado para representar um nao valor
+
